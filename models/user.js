a80a392507bf36e187d673442126546ad0bc4713
haiaza <ajames1342@gmail.com>
2024-08-10 16:57:11 -0400

    foodSchema update

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name: String,
+  delicious: Boolean,
+  required: true,
+})
+
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +16,8 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: foodSchema
+
 });
 
 const User = mongoose.model('User', userSchema);
