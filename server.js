5c206570573d87b9fbcad12d98271f7fc3f12a78
haiaza <ajames1342@gmail.com>
2024-08-10 17:05:54 -0400

    Creating and import food controller

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -28,6 +29,8 @@ app.use(
   })
 );
 
+
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -43,7 +46,15 @@ app.get('/vip-lounge', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use('/users/:userId/foods',foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
+
+//* Routes
+
+app.get('/usersId/foods', async (req, res) => {
+  await res.send('Test complete')
+})
